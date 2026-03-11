d87be15ea564cf7a40e8b7aebdf4fba90e1e6b9b
im24a-thirumala <thirumala@bzz.ch>
2026-03-11 09:29:13 +0100

    Add GameStatus enum to Blackjack game to represent various game states

--- src/main/java/dev/zwazel/springintro/games/blackjack/GameStatus.java
+++ src/main/java/dev/zwazel/springintro/games/blackjack/GameStatus.java
@@ -1,3 +1,4 @@
+package dev.zwazel.springintro.games.blackjack;
 /*
  * Needed before this file:
  * - none
@@ -7,3 +8,15 @@
  * - include turn states and end states
  * - keep names short and consistent
  */
+public enum GameStatus {
+    NOT_STARTED,
+    PLAYER_TURN,
+    DEALER_TURN,
+    PLAYER_BUST,
+    DEALER_BUST,
+    PLAYER_BLACKJACK,
+    PLAYER_WIN,
+    DEALER_WIN,
+    PUSH,
+    GAME_OVER
+}
