a196683dfd0aa258f6b809fa5676165d5e7ef1e9
andris-jacob <andris@jacob.world>
2026-03-25 10:52:23 +0100

    StartGameRequest Class

--- src/main/java/dev/zwazel/springintro/games/blackjack/Hand.java
+++ src/main/java/dev/zwazel/springintro/games/blackjack/Hand.java
@@ -2,7 +2,9 @@ package dev.zwazel.springintro.games.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import dev.zwazel.springintro.games.blackjack.Suit;
+import dev.zwazel.springintro.games.blackjack.Rank;
+import dev.zwazel.springintro.games.blackjack.Card;
 /*
  * Needed before this file:
  * - Card.java
@@ -28,4 +30,14 @@ public class Hand {
         return cards;
     }
 }
+class main {
+    public static void main(String[] args) {
+        Hand playerHand = new Hand();
+        playerHand.addCard(new Card(Suit.HEARTS, Rank.ACE));
+        playerHand.addCard(new Card(Suit.DIAMONDS, Rank.QUEEN));
+        System.out.println(playerHand.getCards());
+    }
+
+}
+
 
